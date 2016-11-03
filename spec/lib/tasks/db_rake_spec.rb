02917add2ad902bb3763ac749ccd5f9f45f8269f
Calaway <Christopher.Calaway@gmail.com>
2016-11-03 13:01:24 -0600

    Work on rake task test.

--- spec/lib/tasks/db_rake_spec.rb
+++ spec/lib/tasks/db_rake_spec.rb
@@ -1,10 +1,13 @@
 require 'rails_helper'
 require 'rake'
 
-describe "db:seed_test_data" do
-  fixture_file = "agricultural_technology.txt"
-  fixture_path = File.join(Rails.root, "data", "test_fixtures_data", fixture_file)
-  Rake::Task[db:seed_test_fixture].invoke(fixture_path)
+RSpec.describe "db" do
+  it "seeds the test database from the fixture data" do
+    fixture_file = "agricultural_technology.txt"
+    fixture_path = File.join(Rails.root, "data", "test_fixtures_data", fixture_file)
+    require "pry"; binding.pry
+    Rake::Task[db:seed_test_fixture].invoke(fixture_path)
 
-  expect(Company.count).to eq(10)
+    expect(Company.count).to eq(10)
+  end
 end
