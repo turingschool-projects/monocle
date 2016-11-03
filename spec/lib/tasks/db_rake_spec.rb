ab7ce6c25a64eb4c2b2a6fb11847ebef1cdaec92
Calaway <Christopher.Calaway@gmail.com>
2016-11-03 13:15:48 -0600

    Got rake task test running with a support/shared_contexts/rake.rb file.

--- spec/lib/tasks/db_rake_spec.rb
+++ spec/lib/tasks/db_rake_spec.rb
@@ -1,12 +1,16 @@
+# Reference: https://robots.thoughtbot.com/test-rake-tasks-like-a-boss
+
 require 'rails_helper'
 require 'rake'
 
-RSpec.describe "db" do
+RSpec.describe "db:seed_test_fixture" do
+  include_context "rake"
+
   it "seeds the test database from the fixture data" do
     fixture_file = "agricultural_technology.txt"
     fixture_path = File.join(Rails.root, "data", "test_fixtures_data", fixture_file)
-    require "pry"; binding.pry
-    Rake::Task[db:seed_test_fixture].invoke(fixture_path)
+
+    subject.invoke(fixture_path)
 
     expect(Company.count).to eq(10)
   end
