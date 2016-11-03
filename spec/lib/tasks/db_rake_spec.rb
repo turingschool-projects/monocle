19c7d4772f038479585576d784e6284311fc6c56
Calaway <Christopher.Calaway@gmail.com>
2016-11-03 11:36:28 -0600

    Stub out test for rake db:seed_test_fixture.

--- spec/lib/tasks/db_rake_spec.rb
+++ spec/lib/tasks/db_rake_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
+require 'rake'
 
 describe "db:seed_test_data" do
-  
+  fixture_file = "agricultural_technology.txt"
+  fixture_path = File.join(Rails.root, "data", "test_fixtures_data", fixture_file)
+  Rake::Task[db:seed_test_fixture].invoke(fixture_path)
+
+  expect(Company.count).to eq(10)
 end
