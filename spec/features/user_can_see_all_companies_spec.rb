ccf668848d9fa2bc4a301a964d3d32317787bcad
Calaway <Christopher.Calaway@gmail.com>
2016-11-03 20:58:09 -0600

    Stub out factory for categories.

---
+++ spec/features/user_can_see_all_companies_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.feature "UserCanSeeAllCompanies", type: :feature do
+  
+  # as a guest,
+  # when I visit the home page,
+
+  # and I click on all companies,
+  # then I see all companies,
+  # and I see all information for that company
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'support/factory_girl'
+
 require 'capybara/rails'
 
 Shoulda::Matchers.configure do |config|
