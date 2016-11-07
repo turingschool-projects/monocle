8a49fa3a21644fdbaf5e40afab49d6c1cec5cba2
Calaway <Christopher.Calaway@gmail.com>
2016-11-06 23:42:00 -0700

    Create users table and User model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161105221802) do
+ActiveRecord::Schema.define(version: 20161107063556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 20161105221802) do
     t.citext   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "slack_uid"
+    t.text     "slack_access_token"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
   end
 
   create_table "zip_codes", force: :cascade do |t|
