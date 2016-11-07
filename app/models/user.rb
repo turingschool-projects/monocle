9086e4441b58b2d43f33587f9bca834004b53af6
Calaway <Christopher.Calaway@gmail.com>
2016-11-07 15:18:13 -0700

    Build #create_from_slack method on User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
                         :slack_uid,
                         :slack_access_token
   validates_uniqueness_of :slack_uid
+
+  def self.create_from_slack(user_info)
+    return false if user_info["ok"] != true #Being explicit to avoid false positives on authentication (i.e. don't allow truthy)
+    user = find_or_initialize_by(slack_uid: user_info["user"]["id"]) do |u|
+      u.username           = user_info["user"]["name"]
+      u.slack_access_token = user_info["access_token"]
+    end
+    user.save ? user : false
+  end
 end
