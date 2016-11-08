1704726d011e2d9f1eb981525b27651fc9c50160
Calaway <Christopher.Calaway@gmail.com>
2016-11-07 18:26:02 -0700

    Work on controllers for creating sessions.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
 class SessionsController < ApplicationController
   def create
     if user = SlackService.authenticate(params)
-
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Signed in Successfully"
     else
 
     end
