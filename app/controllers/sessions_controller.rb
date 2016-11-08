6e6af86369ccd753242c14794cec1982a422ab53
Calaway <Christopher.Calaway@gmail.com>
2016-11-07 20:43:45 -0700

    Add redirect and flash message for failed loggin path on sessions#create.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_path, notice: "Signed in Successfully"
     else
-
+      redirect_to root_path, warning: "Sign in unsuccessful. Please try again."
     end
   end
 end
