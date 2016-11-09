5494032ad96fd4ba815608a5c76bf91d57beed28
Calaway <Christopher.Calaway@gmail.com>
2016-11-08 22:20:43 -0700

    Implement test, guest attempts to visit companies#index.

--- spec/features/guest_user_can_only_visit_root_path_spec.rb
+++ spec/features/guest_user_can_only_visit_root_path_spec.rb
@@ -5,8 +5,21 @@ RSpec.feature "Guest user attempts to visit" do
     scenario "they see the root path" do
       visit root_path
 
+      expect(page).to have_http_status(200)
       expect(page).to have_content("Welcome to Monocle")
       expect(page).to have_link("Sign in with Slack")
+      expect(page).to_not have_css(".navbar")
+      expect(page).to_not have_content("Directory")
+    end
+  end
+
+  context "companies#index" do
+    scenario "they see the root path" do
+      visit companies_path
+
+      expect(page).to have_http_status(404)
+      expect(page).to have_content("The page you were looking for doesn't exist")
+      expect(page).to_not have_css(".navbar")
       expect(page).to_not have_content("Directory")
     end
   end
