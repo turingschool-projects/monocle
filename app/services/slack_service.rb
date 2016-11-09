9610a7922953e30148720a5b69e27cdf9721645b
Calaway <Christopher.Calaway@gmail.com>
2016-11-09 15:02:36 -0700

    Production troubleshooting.

--- app/services/slack_service.rb
+++ app/services/slack_service.rb
@@ -7,6 +7,23 @@ puts "***2*** #{user_info}"
     User.create_from_slack(user_info)
   end
 
+  def self.sign_in_uri
+    "https://slack.com/oauth/authorize?" +
+      "scope=identity.basic,identity.team,identity.avatar&" +
+      "client_id=#{ENV['slack_client_id']}&" +
+      "redirect_uri=#{redirect_uri}"
+  end
+
+  def self.redirect_uri
+    redirect_uri = if ENV["RAILS_ENV"].in?(["test", "development"])
+      "http%3A%2F%2F0.0.0.0%3A3000%2Fauth%2Fslack%2Fcallback"
+    else
+      "https%3A%2F%2Fturingmonocle.herokuapp.com%2Fauth%2Fslack%2Fcallback"
+    end
+puts "***4*** redirect_uri: #{redirect_uri}"
+    redirect_uri
+  end
+
   private
     def self.conn
       Faraday.new(:url => "https://slack.com") do |faraday|
@@ -18,7 +35,7 @@ puts "***2*** #{user_info}"
 
     def self.fetch_info_via_code(code)
       response = conn.get do |req|
-        req.url "/api/oauth.access"
+        req.url "/api/oauth.access?redirect_uri=#{redirect_uri}"
         req.params["code"] = code
       end
 puts "***3*** #{JSON.parse(response.body)}"
