6cd08e89780beeeb3d43a3ba2c952328654ef944
Calaway <Christopher.Calaway@gmail.com>
2016-11-09 17:32:22 -0700

    Add test for different users visiting the home page.

--- spec/features/guest_user_can_only_visit_root_path_spec.rb
+++ spec/features/guest_user_can_only_visit_root_path_spec.rb
@@ -1,18 +1,6 @@
 require 'rails_helper'
 
 RSpec.feature "Guest user attempts to visit" do
-  context "root path" do
-    scenario "they see the root path" do
-      visit root_path
-
-      expect(page).to have_http_status(200)
-      expect(page).to have_content("Welcome to Monocle")
-      expect(page).to have_link("Sign in with Slack")
-      expect(page).to_not have_css(".navbar")
-      expect(page).to_not have_content("Directory")
-    end
-  end
-
   context "companies#index" do
     scenario "they get a 404 page" do
       visit companies_path
