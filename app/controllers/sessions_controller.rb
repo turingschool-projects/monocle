53f35aa9f4011fd02ec7d7385e91ca55e976a97e
Calaway <Christopher.Calaway@gmail.com>
2016-11-10 00:35:57 -0700

    Styling. Add containers and fix flash messages.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,14 +14,14 @@ class SessionsController < ApplicationController
   def create
     if user = SlackService.authenticate(params)
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Signed in successfully."
+      redirect_to root_path, flash: { success: "Signed in successfully." }
     else
-      redirect_to root_path, warning: "Sign in unsuccessful. Please try again."
+      redirect_to root_path, flash: { warning: "Sign in unsuccessful. Please try again." }
     end
   end
 
   def destroy
     reset_session
-    redirect_to root_path, notice: "Signed out successfully."
+    redirect_to root_path, flash: { success: "Signed out successfully." }
   end
 end
