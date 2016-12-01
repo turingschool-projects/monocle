d61a9767bc5545be51d96bfc7fa19643c760cca3
SusiIrwin <susi.irwin@gmail.com>
2016-11-30 17:10:00 -0700

    Add fucntionality to create Business

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -6,32 +6,36 @@ class CompaniesController < ApplicationController
   def show
     @company = Company.find(params[:id])
   end
-  
+
   def new
     @company = Company.new
   end
-  
+
   def create
     @company = Company.new(company_params)
-    binding.pry
     if @company.save
-      redirect_to company_path
+      flash[:notice] = "Company is pending approval."
+      redirect_to company_path(@company)
     else
       flash.now[:danger] = @company.errors.full_messages
       render :new
     end
   end
-  
+
   private
-  
+
   def company_params
-    params.require(:company).permit(:name, 
-                                    :street_address, 
+    params.require(:company).permit(:name,
+                                    :street_address,
                                     :city_state_zip,
-                                    :phone, 
-                                    :website, 
-                                    :headquarters, 
+                                    :phone,
+                                    :website,
+                                    :headquarters,
                                     :products_services,
-                                    :person_in_charge)
+                                    :person_in_charge,
+                                    :city_id,
+                                    :state_id,
+                                    :category_id,
+                                    :zip_code_id)
   end
 end
--- spec/features/user_can_add_a_company_spec.rb
+++ spec/features/user_can_add_a_company_spec.rb
@@ -2,15 +2,17 @@ require 'rails_helper'
 
 describe "user can add a company" do
   it "and then see pending approval" do
-    # As a user,
+
+    state = State.create(name: "Colorado")
+    city = City.create(name: "Denver")
+    zip_code = ZipCode.create(zip_code: "80202")
+    category = Category.create(name: "Applesauce")
+
     user = create(:user)
     stub_login(user)
     visit '/companies'
-    # When I click "Add Company"
     click_on "Add Company"
-    # And I fill in the company name
     fill_in "company_name", with: "Monocle"
-    # And I fill in at least one address for the company
     fill_in "company_street_address", with: "1234 Main St"
     fill_in "company_city_state_zip", with: "Denver, CO, 80203"
     fill_in "company_phone", with: "123-456-789"
@@ -18,13 +20,14 @@ describe "user can add a company" do
     fill_in "company_headquarters", with: "Denver, CO"
     fill_in "company_products_services", with: "Jobs"
     fill_in "company_person_in_charge", with: "Dan Broadbent"
-    # And I click submit
+    find('#company_state_id').find(:xpath, 'option[1]').select_option
+    find('#company_city_id').find(:xpath, 'option[1]').select_option
+    find('#company_zip_code_id').find(:xpath, 'option[1]').select_option
+    find('#company_category_id').find(:xpath, 'option[1]').select_option
+
     click_button "Create Company"
-    # THEN I see a confirmation page
-    
     company = Company.last
     expect(current_path).to eq(company_path(company))
-    # and I see a message saying company is pending approval
-    expect(page).to have_content("Company is pending approval")
+    expect(page).to have_content("Company is pending approval.")
   end
 end
