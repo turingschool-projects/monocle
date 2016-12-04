155aa37ef48b6cba2c8ee6eeec6f63004ab756af
Dan Broadbent <dbroadbent85@gmail.com>
2016-12-04 15:53:34 -0700

    Verify only user who created note can delete it

--- spec/features/acceptance_tests/user_can_delete_own_company_note_spec.rb
+++ spec/features/acceptance_tests/user_can_delete_own_company_note_spec.rb
@@ -19,16 +19,16 @@ describe 'A user edits own note to a company' do
     expect(page).to_not have_content('Note Body')
   end
 
-  # it 'prevents a user from editing another users note' do
-  #   user = create(:user)
-  #   user_2 = create(:user)
-  #   company = create(:company)
-  #   note = create(:note, company_id: company.id, user_id: user.id, title: 'Note Title', body: 'Note Body')
+  it 'prevents a user from editing another users note' do
+    user = create(:user)
+    user_2 = create(:user)
+    company = create(:company)
+    note = create(:note, company_id: company.id, user_id: user.id, title: 'Note Title', body: 'Note Body')
 
-  #   stub_login(user_2)
-  #   visit company_path(company)
-  #   expect(page).to have_content('Note Title')
-  #   expect(page).to have_content('Note Body')
-  #   expect(page).to_not have_css('.edit-button')
-  # end
+    stub_login(user_2)
+    visit company_path(company)
+    expect(page).to have_content('Note Title')
+    expect(page).to have_content('Note Body')
+    expect(page).to_not have_css('.delete-button')
+  end
 end
