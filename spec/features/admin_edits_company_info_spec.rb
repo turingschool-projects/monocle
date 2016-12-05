75319b8c63aff223dce6d4886d4265d041c0d238
Ryan Workman <rdavid1099@gmail.com>
2016-12-04 18:19:29 -0700

    Display multiple addresses for one company on show page

--- spec/features/admin_edits_company_info_spec.rb
+++ spec/features/admin_edits_company_info_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.feature 'admin edits company information' do
   it 'clicks edit button on company page and changes information' do
-    company = create(:company)
+    company = create_approved_company('TestCo')
     admin_logs_in
     visit company_path(company)
     click_on 'Edit Information'
---
+++ spec/features/company_has_multiple_addresses_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe 'User visits company page' do
+  it 'Sees multiple addresses for one company' do
+    company = create_approved_company('TestCo')
+    user_logs_in
+    add_approved_location_to_company(company)
+    visit company_path(company)
+
+    expect(page).to have_content(company.locations.first.street_address)
+    expect(page).to have_content(company.locations.last.street_address)
+  end
+end
