240f66c64029bb99dfbaa4974495bbefccbc843b
Ryan Workman <rdavid1099@gmail.com>
2016-12-04 17:14:57 -0700

    Add functionality, unapproved or rejected companies are not displayed on the company index page

--- app/models/company.rb
+++ app/models/company.rb
@@ -5,4 +5,12 @@ class Company < ApplicationRecord
   has_many :users, through: :starred_companies
   has_many :notes
   has_many :locations
+
+  def self.approved_locations
+    Company.joins(:locations).where('locations.status = ?', '1')
+  end
+
+  def approved?
+    self.locations.any? { |location| location.approved? }
+  end
 end
