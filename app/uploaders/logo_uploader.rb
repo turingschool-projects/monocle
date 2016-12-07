82583c42a80de2e50b144a9481a1fce848dfe8fc
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-12-07 16:00:03 -0700

    Add company logos to views

--- app/uploaders/logo_uploader.rb
+++ app/uploaders/logo_uploader.rb
@@ -17,8 +17,8 @@ class LogoUploader < CarrierWave::Uploader::Base
    #Provide a default URL as a default if there hasn't been a file uploaded:
   def default_url
      # For Rails 3.1+ asset pipeline compatibility:
-     ActionController::Base.helpers.asset_path('images/monocle_icon.png')
-   end
+    ActionController::Base.helpers.asset_path('images/monocle_icon.png')
+  end
 
   # Process files as they are uploaded:
   # process scale: [200, 300]
