8dd63885f08c9103dce32f2f178494538e499e22
Nate Anderson <curtislowe10@gmail.com>
2016-12-11 13:05:38 -0700

    update create and destroy methods in starred_companies controller

--- app/controllers/starred_companies_controller.rb
+++ app/controllers/starred_companies_controller.rb
@@ -4,12 +4,24 @@ class StarredCompaniesController < ApplicationController
   end
 
   def create
-    company = Company.find(params[:company])
+    company = Company.find(params[:company_id])
     current_user.star(company)
+
+    respond_to do |format|
+      redirect_to company_path(company)
+      format.js
+    end
   end
 
   def destroy
     current_user.companies.destroy(params[:id])
-    redirect_to starred_companies_path, flash: { success: "Company removed from starred list" }
+    if request.path == '/starred_companies/*'
+      respond_to do |format|
+        redirect_to starred_companies_path, flash: { success: "Company removed from starred list" } 
+        format.js
+      end
+    else
+      redirect_to :back
+    end
   end
 end
