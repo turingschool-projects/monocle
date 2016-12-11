fd3d082c8f6cda2b5ba6760012f395cce0a392cc
Nate Anderson <curtislowe10@gmail.com>
2016-12-11 13:08:03 -0700

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -16,12 +16,6 @@ ActiveRecord::Schema.define(version: 20161208010246) do
   enable_extension "plpgsql"
   enable_extension "citext"
 
-  create_table "cities", force: :cascade do |t|
-    t.citext   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "companies", force: :cascade do |t|
     t.citext   "name"
     t.citext   "website"
@@ -29,9 +23,9 @@ ActiveRecord::Schema.define(version: 20161208010246) do
     t.citext   "products_services"
     t.datetime "created_at",        null: false
     t.datetime "updated_at",        null: false
+    t.string   "logo"
     t.integer  "status"
     t.citext   "size"
-    t.string   "logo"
   end
 
   create_table "company_industries", force: :cascade do |t|
@@ -78,12 +72,6 @@ ActiveRecord::Schema.define(version: 20161208010246) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "states", force: :cascade do |t|
-    t.citext   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "slack_uid"
@@ -91,12 +79,6 @@ ActiveRecord::Schema.define(version: 20161208010246) do
     t.datetime "created_at",         null: false
     t.datetime "updated_at",         null: false
     t.integer  "role"
-  end
-
-  create_table "zip_codes", force: :cascade do |t|
-    t.string   "zip_code"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "company_industries", "companies"
