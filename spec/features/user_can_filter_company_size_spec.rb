fa4705ad1a765b24ca2960702e792337c0a78324
Nate Anderson <curtislowe10@gmail.com>
2016-12-13 16:24:01 -0700

    add simple-sidebar.css and test for filtering companies with size

---
+++ spec/features/user_can_filter_company_size_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+feature "user can filter company by size" do
+  it "can see all companies with that size" do
+    create_company_with_size("2-10")
+    create_company_with_size("11-50")
+    user_logs_in
+    
+    visit '/'
+    
+    expect(page).to have_content('Company 2-10')
+    expect(page).to have_content('Company 11-50')
+    select "2-10", from: "size"
+    click_on "Submit"
+    expect(page).to have_content('Company 2-10')
+    expect(page).not_to have_content('Company 11-50')
+    
+    
+    
+  end
+end
