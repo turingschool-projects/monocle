ba9f7f4c5e6876c877138e6744b6ed5787d6582f
SusiIrwin <susi.irwin@gmail.com>
2016-12-14 22:11:44 -0700

    Seed the Database with 12 companies, 12 locations, 9 industries. Logos are added. Updated the layout of the Company Show page to have a well to help with usability.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,7 @@ Company.create(id: 1,
                 products_services: "Computer Software",
                 status: "approved",
                 size: "51-200",
-                industry_id: 1)
+                logo: File.new('db/seed_logos/granicus_logo.png'))
 Location.create(street_address: "707 17th Street",
                 street_address_2: "Suite 4000",
                 phone: "720-240-9586",
@@ -19,6 +19,7 @@ Location.create(street_address: "707 17th Street",
                 zip_code: "80202",
                 city: "Denver",
                 state: "CO")
+
 Company.create(id: 2,
                 name: "SpotX",
                 website: "http://www.spotxchange.com",
@@ -26,7 +27,7 @@ Company.create(id: 2,
                 products_services: "Software",
                 status: "approved",
                 size: "201-500",
-                industry_id: 2)              )
+                logo: File.new('db/seed_logos/SpotX_Logo.png'))
 Location.create(street_address: "11030 CirclePoint Road",
                 street_address_2: "Suite 350",
                 phone: "303.345.6650",
@@ -36,6 +37,7 @@ Location.create(street_address: "11030 CirclePoint Road",
                 zip_code: "80020",
                 city: "Denver",
                 state: "CO")
+
 Company.create(id: 3,
                 name: "MondoRobot",
                 website: "www.mondorobot.com",
@@ -43,7 +45,7 @@ Company.create(id: 3,
                 products_services: "Digital Strategies, UI & UX Design, Video & Animation, Mobile & Tablet, Applications, Websites, Virtual Reality, Digital Installations, Augmented Reality, Social Experiences, Branding & Identities, Interactive Games",
                 status: "approved",
                 size: "11-50",
-                industry_id: 3)
+                logo: File.new('db/seed_logos/MondoRobot_logo.png'))
 Location.create(street_address: "5445 Conestoga Court",
                 street_address_2: "suite 200",
                 phone: "303.800.2916",
@@ -53,6 +55,7 @@ Location.create(street_address: "5445 Conestoga Court",
                 zip_code: "80301",
                 city: "Boulder",
                 state: "CO")
+
 Company.create(id: 4,
                 name: "Captain U",
                 website: "http://captainu.com",
@@ -60,7 +63,7 @@ Company.create(id: 4,
                 products_services: "Computer Software",
                 status: "approved",
                 size: "11-50",
-                industry_id: 4)
+                logo: File.new('db/seed_logos/CaptainU.jpg'))
 Location.create(street_address: "910 Santa Fe Dr.",
                 street_address_2: "",
                 phone: "",
@@ -70,6 +73,7 @@ Location.create(street_address: "910 Santa Fe Dr.",
                 zip_code: "80204",
                 city: "Denver",
                 state: "CO")
+
 Company.create(id: 5,
                 name: "Wazee Digital",
                 website: "https://www.wazeedigital.com/",
@@ -77,7 +81,7 @@ Company.create(id: 5,
                 products_services: "cloud-based storage, access, and licensing for enterprise-scale video libraries",
                 status: "approved",
                 size: "51-200",
-                industry_id: 5)
+                logo: File.new('db/seed_logos/Wazee_Digital.png'))
 Location.create(street_address: "1515 Arapahoe Street Tower 3",
                 street_address_2: "Suite 400",
                 phone: "720-382-2869",
@@ -87,6 +91,7 @@ Location.create(street_address: "1515 Arapahoe Street Tower 3",
                 zip_code: "80202",
                 city: "Denver",
                 state: "CO")
+
 Company.create(id: 6,
                 name: "ParkiFi",
                 website: "https://denver.parkifi.com/",
@@ -94,7 +99,7 @@ Company.create(id: 6,
                 products_services: "Real time parking assistance",
                 status: "approved",
                 size: "11-50",
-                industry_id: 6)
+                logo: File.new('db/seed_logos/ParkiFi.png'))
 Location.create(street_address: "1451 Larimer St",
                 street_address_2: "",
                 phone: "724-256-7147",
@@ -112,7 +117,7 @@ Company.create(id: 7,
                 products_services: "Financial Products for the cannabis industry",
                 status: "approved",
                 size: "2-10",
-                industry_id: 7)
+                logo: File.new('db/seed_logos/Tokken_logo.png'))
 Location.create(street_address: "383 Corona Street",
                 street_address_2: "",
                 phone: "720-237-9836",
@@ -122,6 +127,7 @@ Location.create(street_address: "383 Corona Street",
                 zip_code: "80218",
                 city: "Denver",
                 state: "CO")
+
 Company.create(id: 8,
                 name: "GoSpotCheck",
                 website: "http://gospotcheck.com",
@@ -129,7 +135,7 @@ Company.create(id: 8,
                 products_services: "Web apps for field operations",
                 status: "approved",
                 size: "51-200",
-                industry_id: 1)
+                logo: File.new('db/seed_logos/GoSPotCheck_logo.png'))
 Location.create(street_address: "1520 Market St",
                 street_address_2: "",
                 phone: "844-359-2502",
@@ -139,6 +145,7 @@ Location.create(street_address: "1520 Market St",
                 zip_code: "80202",
                 city: "Denver",
                 state: "CO")
+
 Company.create(id: 9,
                 name: "Rachio",
                 website: "http://rachio.com",
@@ -146,7 +153,7 @@ Company.create(id: 9,
                 products_services: "Smart sprinklers",
                 status: "approved",
                 size: "11-50",
-                industry_id: 8)
+                logo: File.new('db/seed_logos/rachio_logo.png'))
 Location.create(street_address: "2040 Larimer St.",
                 street_address_2: "",
                 phone: "",
@@ -157,6 +164,66 @@ Location.create(street_address: "2040 Larimer St.",
                 city: "Denver",
                 state: "CO")
 
+Company.create(id: 10,
+              name: 'FullContact',
+               website: 'fullcontact.com',
+               headquarters: 'Denver, CO',
+               products_services: 'FullContact is the most powerful fully-connected contact management platform for professionals and enterprises who need to master their contacts and be awesome with people.',
+               status: 'approved',
+               size: '51-200',
+              #  industry_id: 10,
+               logo: File.new('db/seed_logos/full_contact_logo.png'))
+Location.create(street_address: '1755 Blake St.',
+                street_address_2: 'Suite #450',
+                phone: '888-330-6943',
+                primary_contact: 'Kaspars Dancis',
+                company_id: 10,
+                status: 'approved',
+                zip_code: '80202',
+                city: 'Denver',
+                state: 'CO')
+
+Company.create(id: 11,
+               name: 'SendGrid',
+               website: 'sendgrid.com',
+               headquarters: 'Denver, CO',
+               products_services: 'Delivers transactional and marketing emails through one reliable platform.',
+               status: 'approved',
+               size: '201-500',
+              #  industry_id: 10,
+               logo: File.new('db/seed_logos/SendGrid-Logo.png'))
+Location.create(street_address: '1801 California St.',
+                street_address_2: 'Suite 500',
+                phone: '888-985-7363',
+                primary_contact: 'Pattie Money',
+                company_id: 11,
+                status: 'approved',
+                zip_code: '80202',
+                city: 'Denver',
+                state: 'CO')
+
+Company.create(id: 12,
+               name: 'CacheMatrix',
+               website: 'cachematrix.com',
+               headquarters: 'Denver, CO',
+               products_services: 'Cachematrix simplifies cash management for leading banks and their corporate clients.',
+               status: 'approved',
+               size: '51-200',
+              #  industry_id: 7,
+               logo: File.new('db/seed_logos/CacheMatrixlogo.png'))
+
+Location.create(street_address: '44 Cook St.',
+                street_address_2: '8th Floor',
+                phone: '303-468-5500',
+                primary_contact: 'Kevin Sun',
+                company_id: 12,
+                status: 'approved',
+                zip_code: '80206',
+                city: 'Denver',
+                state: 'CO')
+
+
+
 Industry.create(id: 1, name: "Computer Software")
 Industry.create(id: 2, name: "Internet")
 Industry.create(id: 3, name: "Marketing and Advertising")
@@ -165,7 +232,6 @@ Industry.create(id: 5, name: "Broadcast Media")
 Industry.create(id: 6, name: "Transportation/Trucking/Railroad")
 Industry.create(id: 7, name: "Financial Software")
 Industry.create(id: 8, name: "Internet of Things")
-Industry.create(id: 9, name: "")
-Industry.create(id: 10, name: "")
-Industry.create(id: 11, name: "")
-Industry.create(id: 12, name: "")
+Industry.create(id: 9,name: 'Contact Management')
+
+User.create(username: "Susi Irwin", role: "admin", census_uid: "6", census_access_token: "94c86789c8d2095da0c213e38e32dc3f4228421e3058f34393ba04643967ebd9")
