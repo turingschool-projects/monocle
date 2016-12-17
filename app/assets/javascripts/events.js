b7a5ac52a9bd4000d7d9bdd4a3c801b934d636a6
Brendan Dillon <br.lyle.dillon@gmail.com>
2016-12-17 12:26:11 -0700

    Refactor starring to use AJAX

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,13 +1,51 @@
 $(document).ready(function(){
-  $("#star").on("click", function(){
-    var companyId = $(this).data('id');
-    return $.ajax({
-      url: "/starred_companies?company=" + companyId,
-      method: "POST"
-    })
-    .done(function(){
-      $("#star").remove();
-      $("table").after('<h4><i class="text-info"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> This company has been saved in your starred list.</i></h4>');
-    })
-  });
+  $(".star").on("click", prepareStar);
+  $(".unstar").on("click", prepareUnstar);
 });
+
+function renderStar() {
+  $('.starred-message').html('');
+  $('.star-toggle').html('<span class="glyphicon glyphicon-star"></span> Star');
+  $('.star-toggle').removeClass('unstar').addClass('star');
+}
+
+function renderUnstar() {
+  $('.starred-message').html('<h4><i class="text-info"><span class="glyphicon glyphicon-star" aria-hidden="true"></span> This company has been saved in your starred list.</i></h4>')
+  $('.star-toggle').html('<span class="glyphicon glyphicon-star"></span> Unstar');
+  $('.star-toggle').removeClass('star').addClass('unstar');
+}
+
+function prepareStar() {
+  var companyId = $(this).data('id');
+  $.ajax({
+    url: "/starred_companies",
+    method: "POST",
+    data: { 'company_id':companyId }
+  })
+  .done(function(){
+    $('.star').off();
+  })
+  .then(function(){
+    renderUnstar();
+  })
+  .then(function(){
+    $(".unstar").on("click", prepareUnstar);
+  })
+}
+
+function prepareUnstar() {
+  var companyId = $(this).data('id');
+  $.ajax({
+    url: "/starred_companies/" + companyId,
+    method: "DELETE"
+  })
+  .done(function(){
+   $(".unstar").off(); 
+  })
+  .then(function(){
+    renderStar();
+  })
+  .then(function(){
+    $(".star").on("click", prepareStar);
+  })
+}
--- app/controllers/starred_companies_controller.rb
+++ app/controllers/starred_companies_controller.rb
@@ -6,11 +6,9 @@ class StarredCompaniesController < ApplicationController
   def create
     company = Company.find(params[:company_id])
     current_user.star(company)
-    redirect_to company_path(company), flash: { success: "Company added to starred list"}
   end
 
   def destroy
     current_user.companies.destroy(params[:id])
-    redirect_back(fallback_location: starred_companies_path, flash: { success: "Company removed from starred list"} ) 
   end
 end
