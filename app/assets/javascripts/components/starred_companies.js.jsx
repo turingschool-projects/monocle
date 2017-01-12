ed192f1a243a8f47acff28ceb8664a1a668e8138
SusiIrwin <susi.irwin@gmail.com>
2017-01-12 11:34:45 -0700

    Remove from DOM

--- app/assets/javascripts/components/starred_companies.js.jsx
+++ app/assets/javascripts/components/starred_companies.js.jsx
@@ -1,20 +1,31 @@
-var StarredCompanies = React.createClass({
-
-  render: function() {
-      return (
-        <div class='card-holder col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2'>
-          <CompanyList companies={ this.props.companies } remove={ this.removeStarredCompany.bind(this) } />
-        </div>
-      )
-  },
-
-  removeStarredCompany(id){
-    this.props.companies = this.props.companies.filter(function(company) {
-      return company.id !== id
-    })
+class StarredCompanies extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      companies: []
+    };
   }
 
-});
+  componentDidMount(){
+    const companies = this.props.companies
+
+    this.setState ({ companies: companies || [] })
+  }
+
+  render() {
+      return (
+        <div className='card-holder col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2'>
+          <CompanyList companies={ this.state.companies } remove={ this.removeStarredCompany.bind(this) } />
+        </div>
+      )
+  }
+
+  removeStarredCompany(id){
+    var starredCompanies = this.state.companies.filter(company => company.id !== id);
+    this.setState({companies: starredCompanies})
+  }
+
+};
 
 const CompanyList = ({companies, remove}) => {
   return(
