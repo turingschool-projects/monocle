69a431d36bac0d67e29ac64980e8cd05ce47cdf6
SusiIrwin <susi.irwin@gmail.com>
2017-01-12 11:08:50 -0700

    Add delete button - still needs functionality

--- app/assets/javascripts/components/starred_companies.js.jsx
+++ app/assets/javascripts/components/starred_companies.js.jsx
@@ -3,27 +3,35 @@ var StarredCompanies = React.createClass({
   render: function() {
       return (
         <div class='card-holder col-xs-12 col-sm-6 col-md-4 col-lg-3 col-xl-2'>
-          <CompanyList companies={ this.props.companies } />
+          <CompanyList companies={ this.props.companies } remove={ this.removeStarredCompany.bind(this) } />
         </div>
       )
+  },
+
+  removeStarredCompany(id){
+    this.props.companies = this.props.companies.filter(function(company) {
+      return company.id !== id
+    })
   }
+
 });
 
-const CompanyList = ({companies}) => {
+const CompanyList = ({companies, remove}) => {
   return(
     <div>
-      { companies.map( company => <Company {...company} />) }
+      { companies.map( company => <Company {...company} remove={ remove } />) }
     </div>
     )
 };
 
-const Company = ({ name, website, products_services, headquarters }) => {
+const Company = ({ name, website, products_services, headquarters, id, remove }) => {
   return(
     <div className='CompanyCard'>
       <h3> { name } </h3>
       <p> { website } </p>
       <p> { products_services } </p>
       <p> { headquarters } </p>
+      <button onClick={()=> remove(id)}>Remove Starred Company</button>
     </div>
   )
 }
