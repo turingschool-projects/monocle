6b3d9ac074b9efc0cc6593e35e9deda9f8e485a5
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-03 19:32:41 -0700

    extracted gmaps code into a js file

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -230,6 +230,7 @@ function convertCompanySize(dropdownValue) {
 }
 
 function placeMapMarker(company, index) {
+  debugger
   this['infowindow' + index] = new google.maps.InfoWindow({
     content: company['name']
   });
@@ -406,4 +407,24 @@ function deleteNote(note) {
 
 function removeNoteHTML(note) {
   $(`#note-${note.id}`).remove()
+}
+
+function initMap() {
+  var coordinates = $('#lat-lng').data('coordinates');
+
+  var map = new google.maps.Map(document.getElementById('company-map'), {
+      center: new google.maps.LatLng(coordinates[0][0], coordinates[0][1]),
+      zoom: 12
+    });
+
+  var bounds = new google.maps.LatLngBounds();
+  coordinates.forEach(function(coordinate){
+    var latLng = new google.maps.LatLng(coordinate[0], coordinate[1]),
+        marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+    bounds.extend(latLng);
+  });
+  map.fitBounds(bounds);
 }
