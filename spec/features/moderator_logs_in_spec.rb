9d1ed57abddebf2c0dbe58d2a11be82f5cb42cce
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-03 17:54:38 -0700

    Change navbar to remove dropdown for regular users

--- spec/features/moderator_logs_in_spec.rb
+++ spec/features/moderator_logs_in_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'moderator logs in' do
   it 'sees moderator tools' do
     moderator_logs_in
     visit root_path
-
+save_and_open_page
     expect(page).to have_content('Moderator Tools')
     expect(page).to_not have_content('Admin Tools')
   end
