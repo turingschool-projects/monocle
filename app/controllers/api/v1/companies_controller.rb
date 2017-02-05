081b01efbb00c520e7b4cda4c10266a17862be2d
Lee Saville <l.w.saville@gmail.com>
2017-02-04 18:30:22 -0700

    Add :zip to the params slice

--- app/controllers/api/v1/companies_controller.rb
+++ app/controllers/api/v1/companies_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CompaniesController < ApplicationController
   def index
-    @companies = Company.approved_companies.filter(params.slice(:company_size, :industry_ids)).includes(:locations)
+    @companies = Company.approved_companies.filter(params.slice(:company_size, :industry_ids, :zip)).includes(:locations)
   end
 end
