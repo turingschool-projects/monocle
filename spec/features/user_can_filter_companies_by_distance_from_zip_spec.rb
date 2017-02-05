5fdeb23f8bcc1310b26a9ee970f0d8f4648034b5
Lee Saville <l.w.saville@gmail.com>
2017-02-04 17:52:09 -0700

    Add spec file for filtering companies by distance from zip

---
+++ spec/features/user_can_filter_companies_by_distance_from_zip_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe 'As a logged in user' do
+
+  before(:each) do
+    user_logs_in
+  end
+
+  context 'when I visit /companies' do
+    VCR.use_cassette('companies_filtering_zip') do
+      scenario 'I can input a zip and only the companies within distance are shown', :js => true do
+        denver_co  = create_denver_company
+        boulder_co = create_boulder_company
+
+        visit '/'
+
+        check('companies_within')
+
+        fill_in 'zip', with: '80203'
+        click_button 'Go'
+
+        expect(page).to have_content(denver_co.street_address)
+        expect(page).to_not have_content(boulder_co.street_address)
+      end
+    end
+  end
+
+end
