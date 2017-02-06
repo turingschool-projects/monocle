5df0a6e4face2fd65f9999789578daad86deee05
Lee Saville <l.w.saville@gmail.com>
2017-02-06 16:57:53 -0700

    Modify first test to explicitly choose 10 miles and add a second test to verify that the distance_select dropdown is functional

--- spec/features/user_can_filter_companies_by_distance_from_zip_spec.rb
+++ spec/features/user_can_filter_companies_by_distance_from_zip_spec.rb
@@ -11,16 +11,37 @@ RSpec.describe 'As a logged in user' do
       scenario 'I can input a zip and only the companies within distance are shown', :js => true do
         denver_co  = create_denver_company
         boulder_co = create_boulder_company
+        co_springs_co = create_colorado_springs_company
 
         visit '/'
 
         check('companies_within')
 
+        select '10', :from => 'distance_select'
         fill_in 'zip', with: '80203'
         click_button 'Go'
 
         expect(page).to have_content(denver_co.locations.first.street_address)
         expect(page).to_not have_content(boulder_co.locations.first.street_address)
+        expect(page).to_not have_content(co_springs_co.locations.first.street_address)
+      end
+
+      scenario 'I can choose from the dropdown and have it filter with a range of those miles', :js => true do
+        denver_co     = create_denver_company
+        boulder_co    = create_boulder_company
+        co_springs_co = create_colorado_springs_company
+
+        visit '/'
+
+        check('companies_within')
+
+        select '100', :from => 'distance_select'
+        fill_in 'zip', with: '80203'
+        click_button 'Go'
+
+        expect(page).to have_content(denver_co.locations.first.street_address)
+        expect(page).to have_content(boulder_co.locations.first.street_address)
+        expect(page).to have_content(co_springs_co.locations.first.street_address)
       end
     end
   end
