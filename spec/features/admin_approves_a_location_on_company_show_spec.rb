6de4c0bceddfb32b6c852d04c18c6f755494c6aa
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-07 11:19:08 -0700

    admin location test passing

--- spec/features/admin_approves_a_location_on_company_show_spec.rb
+++ spec/features/admin_approves_a_location_on_company_show_spec.rb
@@ -5,14 +5,12 @@ RSpec.describe 'admin approves of company location on company show page' do
     VCR.use_cassette('admin_location_approval') do
       admin_logs_in
       company = create_approved_company('Test Company')
-      add_unapproved_location_to_company(company)
+      location = add_unapproved_location_to_company(company)
       visit company_path(company)
-      save_and_open_page
       click_on "Approve"
-      company.reload
 
       expect(page).to have_content('Company location information updated')
-      expect(company.approved?).to eq(true)
+      expect(company.locations.last.approved?).to eq(true)
     end
   end
 end
