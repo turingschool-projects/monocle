d1b2db78e2f923bf21bd53df83c7a9c5ce568113
Chase Dunagan <cdunagan@utexas.edu>
2017-02-07 13:40:02 -0700

    add javascript validation for user creating companies

--- app/assets/javascripts/validations.js
+++ app/assets/javascripts/validations.js
@@ -1,17 +1,66 @@
 $(document).ready( function(){
-  $("#create-company").on('click', checkCompanyFields);
+  // $("#create-company").on('click', checkCompanyFields;
 
 });
 
-function checkCompanyFields(event) {
-    var x, text;
+function checkCompanyFields() {
+
+    var x, y, z, a, b, c, d, e, f, text;
 
     // Get the value of the input field with id="numb"
     x = document.getElementById("location_street_address").value;
+    y = document.getElementById("location_phone").value;
+    z = document.getElementById("location_primary_contact").value;
+    a = document.getElementById("location_city").value;
+    b = document.getElementById("location_zip_code").value;
+    c = document.getElementById("company_website").value;
+    d = document.getElementById("company_headquarters").value;
+    e = document.getElementById("company_products_services").value;
+    f = document.getElementById("company_name").value;
 
     if (x == "") {
-        text = "Input not valid";
-        document.getElementById("demo").innerHTML = text;
-        event.preventDefault()
+        text = "This field is required!";
+        document.getElementById("street_address_req").innerHTML = text;
+        return false
+    }
+    if (y == "") {
+        text = "This field is required!";
+        document.getElementById("phone_req").innerHTML = text;
+        return false
+    }
+    if (z == "") {
+        text = "This field is required!";
+        document.getElementById("primary_contact_req").innerHTML = text;
+        return false
+    }
+    if (a == "") {
+        text = "This field is required!";
+        document.getElementById("city_req").innerHTML = text;
+        return false
+    }
+    if (b == "") {
+        text = "This field is required!";
+        document.getElementById("zip_req").innerHTML = text;
+        return false
+    }
+    if (c == "") {
+        text = "This field is required!";
+        document.getElementById("website_req").innerHTML = text;
+        return false
+    }
+    if (d == "") {
+        text = "This field is required!";
+        document.getElementById("headquarters_req").innerHTML = text;
+        return false
+    }
+    if (e == "") {
+        text = "This field is required!";
+        document.getElementById("services_req").innerHTML = text;
+        return false
+    }
+    if (f == "") {
+        text = "This field is required!";
+        document.getElementById("name_req").innerHTML = text;
+        return false
     }
 };
