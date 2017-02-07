af0d80c923cf4a4c33142c7d3289dfa17aa85965
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-06 17:48:54 -0700

    Remove commented lines of code after refactoring event.js

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -1,8 +1,6 @@
 $(document).ready(function(){
   displayNotes();
-  // bindNoteEvents();
   $("#create-note-button").on('click', prepareNoteCreate);
-  // $("#create-private-note-button").on('click', preparePrivateNoteCreate);
   $(".star").on("click", prepareStar);
   $(".unstar").on("click", prepareUnstar);
   $(".star-job").on("click", prepareJobStar);
@@ -291,12 +289,10 @@ function prepareNoteCreate(){
     var newNoteTitle = $("#create-note-title");
     var newNoteBody = $("#create-note-body");
     var companyName = $("#create-note-company").val();
-    // var userId = $('#create-note-button').data('userId');
-    // var companyId = $('#create-note-button').data('companyId');
-    // var author = $('#create-note-button').data('username');
     var note = { title: newNoteTitle.val(),
                   body: newNoteBody.val(),
                 }
+
     return $.ajax({
       url: "/api/v1/notes",
       method: "POST",
@@ -308,16 +304,12 @@ function prepareNoteCreate(){
 }
 
 function displayNotes(){
-  // var company_id = $('#create-note-button').data('companyId')
   $.ajax({
     url: "/api/v1/notes",
     method: "GET",
     type: "json"
   })
   .then(createNotes)
-  // .then(function(rawNotes){
-  //   rawNotes.forEach(renderNote);
-  // })
 }
 
 function createNotes(raw) {
@@ -336,112 +328,3 @@ function createNotes(raw) {
     note.bindNoteEvents();
   }
 }
-
-// function bindNoteEvents(){
-//     $('#notes').on('click', '#note-1 .delete-button', function() {
-//       deleteNote();
-//     });
-//     // $(`#note-${note.id} .edit-button`).on('click', note.editEvent())
-//   }
-
-// function deleteNote() {
-//     debugger;
-//     // var company_id = $('#create-note-button').data('companyId')
-//     //   $.ajax ({
-//     //     url: `/companies/${company_id}/notes/${this.id}`,
-//     //     type: "DELETE"
-//     //   }).then(removeNoteHTML(note))
-//   }
-
-function renderNote(note){
-  var author = note.author
-  var userId = $('#create-note-button').data('userId')
-  var company_id = $('#create-note-button').data('companyId')
-  if (userId == note.user_id) {
-  $('#notes').append(
-      `<div class='note-block panel panel-default' id="note-${note.id}">
-      <div class='panel-body small'>
-      <div class='btn-group pull-right'>
-      <a class="edit-button btn btn-default btn-sm">Edit</span></a>
-       <a class="delete-button btn btn-default btn-sm"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></a>
-       </div>
-        <div class='pull-left'>
-        <h6>Author:  ${author}</h6>
-        <h6>Title:  <span class="note-title" style="background-color:#fff">${note.title}</span></h6>
-        <h6>Note:  <span class="note-body" style="background-color:#fff">${note.body}</span></h6>
-        </div>
-      </div>
-    </div> `);
-    bindNoteEvents(note)
-  } else {
-      $('#notes').append(
-        `<div class='note-block panel panel-default'>
-          <div class='panel-body small'>
-            <div class='pull-left'>
-              <h6>Author:  ${author}</h6>
-              <h6>Title:  ${note.title}</h6>
-              <h6>Note:  ${note.body}</h6>
-            </div>
-          </div>
-        </div> `
-    )}
-}
-
-// function editEvent(note){
-//   $(`#note-${note.id} .edit-button`).on('click', function(){
-//
-//     $(`#note-${note.id}`).addClass('edit-box')
-//     $(`#note-${note.id}`).find('.note-title').attr('contenteditable', true)
-//     $(`#note-${note.id}`).find('.note-body').attr('contenteditable', true)
-//     $.when(
-//       $(`#note-${note.id} .edit-button`).text('Submit changes')
-//     ).then(
-//       $(`#note-${note.id} .edit-button`).off()
-//     ).then(
-//       $(`#note-${note.id} .edit-button`).on('click', function () {submitChanges(note)})
-//     )
-//   })
-// };
-
-// function submitChanges(note){
-//   var id = note.id
-//   var user_id = $('#create-note-button').data('userId')
-//   var title = $(`#note-${note.id}`).find('.note-title').text();
-//   var body = $(`#note-${note.id}`).find('.note-body').text();
-//   var company_id = $('#create-note-button').data('companyId')
-//   var noteUpdate = { title: title,
-//                        body: body,
-//                        user_id: user_id,
-//                        company_id: company_id}
-//   $.ajax({
-//     url: `/companies/${company_id}/notes/${id}`,
-//     type: "PUT",
-//     data: {note: noteUpdate}
-//   }).then(
-//     $(`#note-${id}`).removeClass('edit-box')
-//   ).then(
-//     $(`#note-${id} .edit-button`).off()
-//   ).then(
-//     $(`#note-${id} .edit-button`).text('Edit')
-//   ).then(
-//     $(`#note-${id} .edit-button`).on('click', editEvent(note))
-//   )
-// }
-
-// function bindNoteEvents(note){
-//   $(`#note-${note.id} .delete-button`).on('click', function(){deleteNote(note)})
-//   $(`#note-${note.id} .edit-button`).on('click', editEvent(note))
-// }
-
-// function deleteNote(note) {
-//   var company_id = $('#create-note-button').data('companyId')
-//   var id = note.id
-//     $.ajax ({
-//       url: `/companies/${company_id}/notes/${id}`,
-//       type: "DELETE"
-//     }).then(removeNoteHTML(note))
-// }
-
-// function removeNoteHTML(note) {
-//   $(`#note-${note.id}`).remove()
-// }
