df3a06204b1e5732dc86fab701914c53444e3145
Lee Saville <l.w.saville@gmail.com>
2017-02-09 09:31:02 -0700

    Add method for company_size_options

--- app/models/company_size.rb
+++ app/models/company_size.rb
@@ -2,6 +2,16 @@ module CompanySize
 
   def company_size_options
     [
+      '2-10',
+      '11-50',
+      '51-200',
+      '201-500',
+      '501-1000',
+    ]
+  end
+
+  def company_size_filter_options
+    [
       'All',
       '10',
       '50',
@@ -13,4 +23,5 @@ module CompanySize
       '10001+'
     ]
   end
+
 end
