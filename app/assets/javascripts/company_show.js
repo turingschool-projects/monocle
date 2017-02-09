0710b8561bb14403780c1e108d75396e8dc1e1d0
Alisher Sadikov <alishersadikov@gmail.com>
2017-02-08 21:12:40 -0700

    hit api with ajax to fetch coordinates

--- app/assets/javascripts/company_show.js
+++ app/assets/javascripts/company_show.js
@@ -1,5 +1,9 @@
 function initCompanyMap() {
-  var coordinates = $('#lat-lng').data('coordinates');
+
+  var companyId = $('#company-id').data('companyId');
+
+  var coordinates = getCoordinates();
+  debugger;
 
   var map = new google.maps.Map(document.getElementById('company-map'), {
       center: new google.maps.LatLng(coordinates[0][0], coordinates[0][1]),
@@ -9,12 +13,14 @@ function initCompanyMap() {
   var bounds = new google.maps.LatLngBounds();
 
   coordinates.forEach(function(coordinate){
-    var latLng = new google.maps.LatLng(coordinate[0], coordinate[1]),
-        marker = new google.maps.Marker({
-      position: latLng,
-      map: map
-    });
-    bounds.extend(latLng);
+    if (coordinate !== undefined) {
+      var latLng = new google.maps.LatLng(coordinate.lat, coordinate.lng),
+          marker = new google.maps.Marker({
+        position: latLng,
+        map: map
+      });
+      bounds.extend(latLng);
+    }
   });
 
   map.fitBounds(bounds);
@@ -24,4 +30,27 @@ function initCompanyMap() {
           this.setZoom(9);
       }
     });
+}
+
+function getCoordinates() {
+  var companyId = $('#company-id').data('companyId');
+
+  var locations = $.parseJSON($.ajax({
+    url: `/api/v1/companies/${companyId}/locations`,
+    method: "GET",
+    data: {'company_id': companyId},
+    type: "json",
+    async: false
+  }).responseText);
+
+  return extractCoordinates(locations);
+}
+
+function extractCoordinates(locations) {
+  var coordinates = locations.map(function(location){
+    if (location.status === "approved") {
+      return JSON.parse(location.coordinates)
+    }
+  })
+  return coordinates;
 }
