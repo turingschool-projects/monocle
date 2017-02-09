b1e53621458558e14752f21f7748361191f12260
Lee Saville <l.w.saville@gmail.com>
2017-02-08 18:44:50 -0700

    Add logic to toggleSizeSelect to refilter on unselect

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -201,6 +201,10 @@ function appendNoCompaniesNotice() {
 
 function toggleSizeSelect() {
   $('#sizes').toggle()
+  if(!$(this).is(':checked')) {
+    $('#sizes').val('All');
+    filterCompanies();
+  }
 }
 
 function removeCards() {
