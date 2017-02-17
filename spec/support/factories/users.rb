b7af8e64afa79d85543e623358411591c857d5a6
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-17 15:44:01 -0700

    Remove slack references from factory girl

--- spec/support/factories/users.rb
+++ spec/support/factories/users.rb
@@ -1,7 +1,5 @@
 FactoryGirl.define do
   factory :user do
     username { Faker::Internet.user_name }
-    slack_uid { SecureRandom.hex(4) }
-    slack_access_token "xxxx-xxxxxxxxxx-xxxxxxxxxxx-xxxxxxxxxxxx-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
   end
 end
