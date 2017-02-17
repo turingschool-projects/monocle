ff6978413bfded4211b3ef38db931b30482ccd4e
Lee Saville <l.w.saville@gmail.com>
2017-02-16 19:20:43 -0700

    Change flash message to be more specific

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -25,8 +25,7 @@ class CompaniesController < ApplicationController
       flash[:notice] = "Company is pending approval."
       redirect_to company_path(@company)
     else
-      # flash.now[:danger] = @company.errors.full_messages
-      flash[:danger] = "Missing required fields"
+      flash[:danger] = "Missing required name field"
       redirect_to new_company_path
     end
   end
