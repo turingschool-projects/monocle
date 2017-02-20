d37be40670d33141aa9912c28e1a0bea874c4c91
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-20 08:01:30 -0700

    Start create action on admin/employeescontroller

--- app/controllers/api/v1/admin/employees_controller.rb
+++ app/controllers/api/v1/admin/employees_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::Admin::EmployeesController < ApplicationController
+
+  skip_before_action :verify_authenticity_token
+
+  def create
+    user = User.find_by(username: "#{params[:first_name]} #{params[:last_name]}")
+    company = Company.find(params[:company_id])
+    employee = Employee.new(
+      first_name: params[:first_name],
+      last_name: params[:last_name]
+    )
+    require "pry"; binding.pry
+  end
+
 end
--- spec/requests/api/v1/admin/admin_adds_employee_spec.rb
+++ spec/requests/api/v1/admin/admin_adds_employee_spec.rb
@@ -1,16 +1,24 @@
 require 'rails_helper'
 
-RSpec.describe 'api/v1/companies/:id/admin/employees' do
-  it "should add an employee" do
-    admin_logs_in
-    user = User.first
-    company = create(:company)
+RSpec.describe 'api/v1/companies/:id/admin' do
+  context 'POST /api/v1/companies/:id/admin/employees' do
+    it "should add an employee" do
+      admin_logs_in
+      user = create(:user)
+      first_name = user.username.split(' ').first
+      last_name = user.username.split(' ').last
+      company = create(:company)
 
-    post "/api/v1/companies/#{company.id}/admin/employees"
+      params = {
+        first_name: first_name,
+        last_name: last_name
+      }
 
-    expect(response).to be_success
-    expect(company.employees.first).to eq(user)
-  end
+      post "/api/v1/companies/#{company.id}/admin/employees", params: params
 
+      expect(response).to be_success
+      expect(company.employees.first.first_name).to eq(first_name)
+      expect(company.employees.first.last_name).to eq(last_name)
+    end
   end
 end
