3fd9adecc32fd1f503756e5a338f531cccbd2449
Michael Scherer <michael.c.scherer@gmail.com>
2017-02-20 08:40:07 -0700

    Remove census user migration

--- app/models/census_user.rb
+++ app/models/census_user.rb
@@ -2,15 +2,19 @@ class CensusUser
 
   def self.create_census_users(url, access_token)
     census_results = CensusService.new.get_users(url, access_token)
-    census_results.each do |census_user|
-      CensusUser.new(
-        census_user[:id]
-        census_user[:first_name],
-        census_user[:last_name],
-        census_user[:slack],
-        census_user[:email]
-      )
+    census_results.find do |census_user|
+      require "pry"; binding.pry
+      census_user
     end
+    # census_results.each do |census_user|
+    #   CensusUser.new(
+    #     census_user[:id]
+    #     census_user[:first_name],
+    #     census_user[:last_name],
+    #     census_user[:slack],
+    #     census_user[:email]
+    #   )
+    # end
   end
 
 end
--- db/migrate/20170220150508_create_census_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateCensusUsers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :census_users do |t|
-
-      t.timestamps
-    end
-  end
-end
